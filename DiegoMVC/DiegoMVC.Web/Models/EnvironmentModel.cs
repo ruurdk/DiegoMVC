d4083e9d762b1949325ab2ba6441466e6e4cf8ba
ruurdk <ruurdk@hotmail.com>
2015-08-26 14:43:00 +0200

    untracked files

---
+++ DiegoMVC/DiegoMVC.Web/Models/EnvironmentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiegoMVC.Web.Models
+{
+    public class EnvironmentModel
+    {
+        public Guid ApplicationId { get; set; }
+        public IEnumerable<KeyValuePair<string, string>> Variables { get; set; }       
+    }
+}
